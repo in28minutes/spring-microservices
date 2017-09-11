0d3f97deefc3bbfb00ceea48c2f3f323e389c78c
Ranga Rao Karanam <rangaraokaranam@Rangas-MacBook-Pro.local>
2017-09-11 11:10:22 +0530

    Distributed Tracing Done

--- 03.microservices/step41/currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValueRepository.java
+++
@@ -1,8 +0,0 @@
-package com.in28minutes.microservices.currencyexchangeservice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExchangeValueRepository extends 
-		JpaRepository<ExchangeValue, Long>{
-	ExchangeValue findByFromAndTo(String from, String to);
-}
