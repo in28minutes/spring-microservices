a10bda506baacde826f7b07d0c6ef39ab41dd10d
Ranga Karanam <rangakaranam@Rangas-MacBook-Air.local>
2019-10-28 14:48:44 +0530

    Thank You for Choosing to Learn from in28Minutes

--- 03.microservices/step34/currency-exchange-service/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
-values(10001,'USD','INR',65,0);
-insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
-values(10002,'EUR','INR',75,0);
-insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
-values(10003,'AUD','INR',25,0);
