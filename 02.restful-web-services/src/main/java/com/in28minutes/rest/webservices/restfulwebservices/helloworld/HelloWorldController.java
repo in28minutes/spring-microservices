bb4a386806e1816d1d0b1c786ea45c1b7d77ba58
Razafinirina <Josdor.r@gmail.com>
2022-03-17 22:24:55 +0100

    ajout lombok

--- 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
 import java.util.Locale;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
+@Slf4j
 @RestController
 public class HelloWorldController {
 	
@@ -30,6 +32,7 @@ public class HelloWorldController {
 	///hello-world/path-variable/in28minutes
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		log.info("Hello Word with PathVariable");
 		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 
